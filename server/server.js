feed426ce786f257ff547a9bc273223b89869e3e
Shawn Lenhardt <shawn.lenhardt@gmail.com>
2018-02-28 12:11:39 -0500

    Add tests for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -27,6 +28,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (error) => {
     res.status(400).send(error);
+  });
+});
+
+// GET /toods/12345
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({
+      todo: todo
+    });
+  }).catch((error) => {
+    res.status(400).send();
   });
 });
 
